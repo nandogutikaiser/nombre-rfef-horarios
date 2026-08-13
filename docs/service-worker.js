b3d9d052991b99e7c4f37c669ea012e303ca175b
nandogutikaiser <Fernandososabarrado@hotmail.es>
2026-08-13 09:11:13 +0200

    Add files via upload

---
+++ docs/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE = "horarios-rfef-v1";
+const SHELL = ["./", "./index.html", "./manifest.json", "./data/horarios.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(event.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
